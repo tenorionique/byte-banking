71bfec26a5fc2eedf233300c6d8309b48b3eb87b
monique <monique.sa@raizen.com>
2022-07-18 08:16:17 -0300

    02-Comportamentos de classes

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 class Cliente {
     nome;
     cpf;
+}
+class ContaCorrente {
     agencia;
     saldo;
-    rg;
+    sacar(valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = "11122233309";
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg= 12345678;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = "88822233309";
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrente()
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+console.log(contaCorrenteRicardo.saldo);
+
+contaCorrenteRicardo.saldo = 100
+console.log(contaCorrenteRicardo.saldo);
+
+contaCorrenteRicardo.sacar(50);
+console.log(contaCorrenteRicardo.saldo);
+
+console.log(cliente1);
+console.log(cliente2)
